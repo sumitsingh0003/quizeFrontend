2aa3cb37c8442e9c67692268ca652bf7bae4a064
Sumit Singh <Sumit18440@gmail.com>
2024-03-01 17:12:30 +0530

    Your commit message here

--- src/Components/CongratsPopup/CongratsPopup.js
+++ src/Components/CongratsPopup/CongratsPopup.js
@@ -1,11 +1,12 @@
 // CongratsPopup.js
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import styles from './CongratsPopup.module.css';
-const baseUrl = process.env.REACT_APP_BASE_URL || '';
+// const baseUrl = process.env.REACT_APP_BASE_URL || '';
 
 const CongratsPopup = ({ quizLink }) => {
 
 const [showCopyLink, setShowCopyLink] = useState(false);
+const [domainLink, setDomainLink] = useState('');
 
   const handleCopyLink = (link) => (event) => {
     event.preventDefault();
@@ -17,13 +18,21 @@ const [showCopyLink, setShowCopyLink] = useState(false);
    
   };
 
+const getDomainUrl = () =>{
+  const domainURL =window.location.protocol + "//" + window.location.hostname + (window.location.port ? ":" + window.location.port : "");
+  setDomainLink(domainURL)
+}
+
+  useEffect(() => {
+    getDomainUrl();
+  }, []);
 
   return (
     <div className={styles.congratsPopup}>
       <p>Congrats! Your Quiz is Published!</p>
      
       <div className={styles.copyData}>
-        <p>{baseUrl}/quiz/{quizLink}</p><button  onClick={handleCopyLink(`${baseUrl}/quiz/${quizLink}`)}>Share</button>
+        <p>{domainLink}/quiz/{quizLink}</p><button  onClick={handleCopyLink(`${domainLink}/quiz/${quizLink}`)}>Share</button>
       </div>
       {showCopyLink && (
           <div className={styles.copiedBox}>
--- src/constant/baseurl.js
+++ src/constant/baseurl.js
@@ -1,3 +1,3 @@
-// const BASEURL ="https://quiz-backend-29g5.onrender.com";
-const BASEURL= "http://localhost:4000";
+const BASEURL ="https://quizebackend.onrender.com";
+// const BASEURL= "http://localhost:4000";
 export default BASEURL;
